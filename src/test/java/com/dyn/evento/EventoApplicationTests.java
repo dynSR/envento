abc47d58724a28d22133a8399c2e0407f52d5cd3
dynSR <dylan.schumacker+github@outlook.fr>
2025-05-12 11:29:13 +0200

    Added Spring and Angular projects

---
+++ src/test/java/com/dyn/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyn.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
